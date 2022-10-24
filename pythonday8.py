cb5cb551ef520d1410d37d44d7d3a068fe9aa8e0
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-24 16:54:38 +0530

    Add files via upload

---
+++ pythonday8.py
@@ -0,0 +1,60 @@
+
+x = (10)
+y = int(2)
+z = float(10)
+print(x, y, z)
+print(x+y)
+# exercise
+x = 78
+y = ("eren yeger")
+print(type(x))
+print(type(y))
+print(y.replace("eren", "EREN"))
+# slice string
+x = "great power comes great responsibilty"
+y = "so go for it and do domething which is always good"
+z = "thats what really men do"
+print(x.upper())
+print(x.lower())
+print(y.lower())
+print(y.upper())
+print(z.upper())
+print(z.lower())
+
+print(type(z))
+print(type(x))
+print(type(y))
+print(x.replace("e", "o"))
+print(y.replace("r", "u"))
+print(y.replace("k", "p"))
+print(y.replace("h", "k"))
+print(y.replace("l", "r"))
+print(y.replace("y", "t"))
+print(y.replace("a", "q"))
+print(y.replace("t", "c"))
+print(y.replace("d", "k"))
+print(y.replace("k", "t"))
+print(x.split())
+print(x.split())
+# mylist properties
+mylist = [40, 20, 43, 56, 98, 89, 90, 87]
+mylist[1:2] = [58, 300]
+mylist[1:5] = [2000, 300]
+mylist[0:4] = [200, 300]
+mylist[0:6] = [200, 3000]
+mylist[1:3] = [200, 300]
+mylist[-6:2] = [200, 300]
+mylist[1:-8] = [200, 300]
+mylist[1:-6] = [200, 300]
+mylist[-1:6] = [200, 300]
+print(mylist)
+mylist.clear()
+print(mylist)
+print(100+103)
+print(1056-202)
+print(1090-207)
+print(107*207)
+print(1066/201)
+print(105//201)
+print(1051 % 220)
+print(109**209)
